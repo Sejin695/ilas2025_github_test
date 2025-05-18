a1d917c87ea549b858121b981fa914f13e41edc5
sejin <hansejin345@gmail.com>
2025-05-18 18:22:36 +0900

    loop

--- nguess.py
+++ nguess.py
@@ -1,14 +1,15 @@
 import random
 
-guess = int(input('guess='))
-print('Your guess is', guess)
 answer = random.randint(1, 100)
-if guess == answer :
-    print('Good guess')
-elif guess < answer:
-    print('Too low')
-else:
-    if (guess < answer):
+
+while True:
+    guess = int(input('guess='))
+    print('Your guess is', guess)
+
+    if guess == answer:
+        print('Good guess! You got it right.')
+        break
+    elif guess < answer:
         print('Too low')
     else:
         print('Too high')
